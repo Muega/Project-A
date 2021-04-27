9df9ed26e114f6ccda78afd1a5429b64fcb9603e
Daria <d_litwinienko@gmx.de>
2021-04-27 09:12:17 +0200

    hier wurde ejs installiert (nicht wundern); und body-parser installiert; entsprechende Initialisierungen sind im Serverskript

--- main/server.js
+++ main/server.js
@@ -8,17 +8,28 @@ npm install express --save
 -""- express-fileupload
 */ 
 
-//Im Folgenden die Initialisierungen der Module: 
+//Im Folgenden die Initialisierungen der Module express, body-parser, ejs: 
 const express = require("express");
 const app = express();
 
+//Intitialisierung Body-Parser 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Initialisierung EJS
+app.engine(".ejs", require("ejs").__express); //das Modul mit der Variante für express wird geladen
+app.set("view engine", "ejs");
+
 app.use(express.static(__dirname + "/public"));
+
 
 //Server starten:
 app.listen(3000, function(){
     console.log("listening on 3000");
 });
 
+
+//Hier sind unsere Pages:
 app.get("/home", function(req, res){
     res.sendFile(__dirname + "/views/home.html");
 });
