cb7882b7e0af3f0756517383401fe5597c52e185
Daria <d_litwinienko@gmx.de>
2021-04-27 09:06:38 +0200

    habe ejs installiert und ein paar kommentare im serverskript gelassen

--- main/server.js
+++ main/server.js
@@ -1,10 +1,22 @@
+/* 
+first steps as always: 
+npm init
+npm install express --save
+-""- ejs
+-""- body-parser
+-""- sqlite3
+-""- express-fileupload
+*/ 
+
+//Im Folgenden die Initialisierungen der Module: 
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/public/.."));
+app.use(express.static(__dirname + "/public"));
 
+//Server starten:
 app.listen(3000, function(){
-    console.log("listining on 3000");
+    console.log("listening on 3000");
 });
 
 app.get("/home", function(req, res){
